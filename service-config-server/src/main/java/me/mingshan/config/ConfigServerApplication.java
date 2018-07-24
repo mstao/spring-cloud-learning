6623de51f91c7f18a1d2aaab485e502802ed7f0d
Juntao Han <499445428@qq.com>
2018-07-25 00:24:18 +0800

    Update service-config-server

--- service-config-server/src/main/java/me/mingshan/config/ConfigServerApplication.java
+++ service-config-server/src/main/java/me/mingshan/config/ConfigServerApplication.java
@@ -1,11 +1,15 @@
 package me.mingshan.config;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-0
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
