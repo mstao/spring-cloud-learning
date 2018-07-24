7db7fa94e00db3213e9ce07a011c3ace57c87df4
Juntao Han <499445428@qq.com>
2018-07-25 00:05:31 +0800

    Add service config.

--- service-config-server/src/main/java/me/mingshan/config/ConfigServerApplication.java
+++ service-config-server/src/main/java/me/mingshan/config/ConfigServerApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
-@EnableConfigServer
+0
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
