6f994f519ba0e351dcc7803d6eafae77b848ba8b
Juntao Han <499445428@qq.com>
2018-07-24 22:01:14 +0800

    Add config-client.

---
+++ config-client/src/main/java/me/mingshan/config/ConfigClientApplication.java
@@ -0,0 +1,25 @@
+package me.mingshan.config;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ConfigClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+    @Autowired
+    JdbcConfigBean jdbcConfigBean;
+
+    @RequestMapping(value = "/hi")
+    public String hi(){
+        return jdbcConfigBean.toString();
+    }
+}
